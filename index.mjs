eb3a2609fa796343bc1dad5e6a97b8c2a6660847
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-03-02 03:14:28 +0000

    added express server boilerplate

--- index.mjs
+++ index.mjs
@@ -1,1 +1,22 @@
-console.log("Hello World");
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+
+
+
+// Render Html File
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+app.listen(port, () => {
+  // Code.....
+})
