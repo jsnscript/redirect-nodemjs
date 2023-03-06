d8a32980e8fc400f2c388ed4b988cd979e89d847
weblette <127142905+weblette@users.noreply.github.com>
2023-03-06 15:28:56 -0600

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,20 +3,70 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
-const app = express();
-const port = 3000;
+const hostTarget = 'patrick-ring-motive.github.io';
 
+http.createServer(onRequest).listen(3000);
 
-
-// Render Html File
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+async function onRequest(req, res) {
+  req.headers.host = hostTarget;
+  req.headers.referer = hostTarget;
 
 
-app.listen(port, () => {
-  // Code.....
-})
+
+  /* start reading the body of the request*/
+  let bdy = "";
+  req.on('readable', function() {
+    bdy += req.read();
+  });
+  req.on('end', async function() {
+    /* finish reading the body of the request*/
+
+    /* start copying over the other parts of the request */
+    let options = {
+      method: req.method,
+      headers: req.headers
+    };
+    /* fetch throws an error if you send a body with a GET request even if it is empty */
+    if ((req.method != 'GET') && (req.method != 'HEAD') && (bdy.length > 0)) {
+      options = {
+        method: req.method,
+        headers: req.headers,
+        body: bdy
+      };
+    }
+    /* finish copying over the other parts of the request */
+
+    /* fetch from your desired target */
+    let response = await fetch('https://' + hostTarget + req.url, options);
+
+    /* copy over response headers 
+
+    
+    */
+
+
+    res.headers = response.headers;
+
+    /* check to see if the response is not a text format */
+    let ct = response.headers.get('content-type');
+
+    if ((ct) && (ct.indexOf('image') == -1) && (ct.indexOf('video') == -1) && (ct.indexOf('audio') == -1)) {
+
+      /* Copy over target response and return */
+      let resBody = await response.text();
+      res.end(resBody);
+
+
+    } else {
+
+      /* if not a text response then redirect straight to target */
+      res.setHeader('location', 'https://' + hostTarget + req.url);
+      res.statusCode = 302;
+      res.end();
+
+    }
+  });
+
+
+}
